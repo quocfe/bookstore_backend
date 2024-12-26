60870a33d5d6b9ee5886ef7a11e41823fe20e280
Quoc <phuquocfe@gmail.com>
2024-12-26 11:10:17 +0700

    fix connect db

--- src/config/connect.js
+++ src/config/connect.js
@@ -3,10 +3,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const connection = mysql.createConnection({
-	database: process.env.DATABASE,
+	host: process.env.HOST,
 	user: process.env.DB_USERNAME,
-	password: process.env.PASSWORD,
-	port: process.env.PORT,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DATABASE,
 });
 
 export default connection;
